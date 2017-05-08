7a0e9c7bea32c96b1b900d10174a220c8e17625a
Aesonne17 <algor-aesonne@yandex.ru>
2017-05-08 14:41:56 +0300

    Backend. Server's build: - add module with Tomcat and script for building it and artefacts' deploying; - update Readme.

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 Application for storing photos, creating photo galleries, slide shows and collages
 
 For build and deploy a project used:
-- jdk (version 1.8.0_112);
-- maven (version 3.3.9).
+- jdk (version 1.8.0_131);
+- maven (version 3.5.0);
+- python (version 3.5.2) - for building server only.
 
 Build project:
-- run "mvn clean install" from the root package of the project to build all project modules.
+- change settings in the config for building script: /dev-tools/src/resources/build-server-config.txt;
+- run build-server.py.
+
+For run project:
+- start server:
+Linux: in server_home_folder/gallery-tomcat/bin made all sh files executable; start server from that folder
+with command "./catalina run";
+Windows: in folder server_home_folder/gallery-tomcat/bin use dblclick on file startup.bat;
+- in the address bar of any browser type: "http://localhost:8080/rest-services/login".
---
+++ dev-tools/src/main/py/build-server.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import os
+import shutil
+import zipfile
+import subprocess
+
+print ("The project is launched...")
+
+#set environment
+print ("Setting environment data...")
+PWD_DIRECTORY=os.path.abspath(__file__)
+SOURCE_DIRECTORY=os.path.join(PWD_DIRECTORY[:-23], "resources")
+MVN_SOURSE_DIRECTORY=PWD_DIRECTORY[:-37]
+TOMCAT_NAME="gallery-tomcat"
+TOMCAT_ARCHIVE_NAME=TOMCAT_NAME + ".zip"
+TOMCAT_WAR_FOLDER="webapps"
+TOMCAT_JAR_FOLDER=os.path.join("lib", "common")
+TARGET_FOLDER="target"
+
+#build project
+print ("Building project...")
+#TODO: deal with the problems of this parameter in different OS
+subprocess.call(["mvn", "clean", "install"], cwd=MVN_SOURSE_DIRECTORY, shell=True)
+#subprocess.call(["mvn", "clean", "install"], cwd=MVN_SOURSE_DIRECTORY)
+
+#parse config
+print ("Scanning configuration file...")
+fileConfig = open(os.path.join(SOURCE_DIRECTORY, "build-server-config.txt"))
+for line in fileConfig:
+    if line.startswith("SERVER_DIRECTORY"):
+        SERVER_DIRECTORY=line[17:]
+fileConfig.close()
+SERVER_FULL_PATH=os.path.join(SERVER_DIRECTORY, TOMCAT_NAME)
+print ("The server will be installed in the directory: " + SERVER_DIRECTORY)
+
+#build server
+print ("Server building...")
+if not os.path.exists(SERVER_DIRECTORY):
+    os.makedirs(SERVER_DIRECTORY)
+elif os.path.exists(SERVER_FULL_PATH):
+    shutil.rmtree(SERVER_FULL_PATH)
+shutil.copy(os.path.join(SOURCE_DIRECTORY, TOMCAT_ARCHIVE_NAME), os.path.join(SERVER_DIRECTORY, TOMCAT_ARCHIVE_NAME))
+zipfile.ZipFile(os.path.join(SERVER_DIRECTORY, TOMCAT_ARCHIVE_NAME), 'r').extractall(SERVER_DIRECTORY)
+os.remove(os.path.join(SERVER_DIRECTORY, TOMCAT_ARCHIVE_NAME))
+
+#copying artifact
+print ("Deploying app...")
+shutil.copy(os.path.join(MVN_SOURSE_DIRECTORY, "dao", TARGET_FOLDER, "dao-1.0-SNAPSHOT.jar"),
+os.path.join(SERVER_FULL_PATH, TOMCAT_JAR_FOLDER, "dao.jar"))
+shutil.copy(os.path.join(MVN_SOURSE_DIRECTORY, "auth", TARGET_FOLDER, "auth-1.0-SNAPSHOT.jar"),
+os.path.join(SERVER_FULL_PATH, TOMCAT_JAR_FOLDER, "auth.jar"))
+shutil.copy(os.path.join(MVN_SOURSE_DIRECTORY, "photo-core", TARGET_FOLDER, "photo-core-1.0-SNAPSHOT.jar"),
+os.path.join(SERVER_FULL_PATH, TOMCAT_JAR_FOLDER, "photo-core.jar"))
+shutil.copy(os.path.join(MVN_SOURSE_DIRECTORY, "rest-services", TARGET_FOLDER, "rest-services-1.0-SNAPSHOT.war"),
+os.path.join(SERVER_FULL_PATH, TOMCAT_WAR_FOLDER, "rest-services.war"))
+
+print ("The project was launched successfully")
